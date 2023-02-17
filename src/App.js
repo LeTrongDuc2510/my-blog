0422734bddd8413e299a0f0291f57424cc3aac2f
LeTrongDuc2510 <leducpro25102003@gmail.com>
2023-02-17 23:05:57 +0700

    some mystery bugs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-//import React from "react";
+import React from "react";
 import Navigation from "./components/navigation";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import PageRenderer from "./page-renderer";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Routes>
+          <Route path="/:page" component={PageRenderer} />
+          <Route path="/" render={() => <Navigate to="/home" />} />
+          <Route component={() => 404} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/page-renderer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useMatch } from "react-router-dom";
+
+const generatePage = (page) => {
+  const component = () => require(`./pages/${page}.js`).default;
+  try {
+    return React.createElement(component());
+  } catch (err) {
+    console.warn(err);
+    return React.createElement(() => 404);
+  }
+};
+
+export default function PageRenderer() {
+  const {
+    params: { page },
+  } = useMatch();
+
+  return generatePage(page);
+}
